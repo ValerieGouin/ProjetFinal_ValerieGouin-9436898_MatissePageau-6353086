c38d713b3b28b73267fa660efd0241c8ec3322d5
maladarix <maladarix@gmail.com>
2024-11-27 14:41:23 -0500

    Update donnees.js

--- Code/src/convertionTypes/donnees.js
+++ Code/src/convertionTypes/donnees.js
@@ -1,87 +1,86 @@
 class donnees {
-
-  ToGigaoctet (value, fromUnit) { //Convert all to Go
-    let result = 0
+  ToGigaoctet(value, fromUnit) { // Convert all to Go
+    let result = 0;
     switch (fromUnit) {
       case "bit":
-        result = value / (8 * 10 ** 9)
+        result = value / (8 * 1_073_741_824); // Binary: 1 Go = 8 * 2^30 bits
         break;
       case "octet":
-        result = value / 10**9
-        break
+        result = value / 1_073_741_824; // Binary: 1 Go = 2^30 octets
+        break;
       case "kilooctet":
-        result = value / 10**6
-        break
+        result = value / (1_024 * 1_024); // Binary: 1 Go = 2^20 kilooctets
+        break;
       case "megaoctet":
-        result = value / 10**3
-        break
+        result = value / 1_024; // Binary: 1 Go = 2^10 megaoctets
+        break;
       case "gigaoctet":
-        result = value
-        break
+        result = value; // Already in gigaoctets
+        break;
       case "terraoctet":
-        result = value * 10**3
-        break
+        result = value * 1_024; // Binary: 1 To = 2^10 Go
+        break;
       case "petaoctet":
-        result = value * 10**6
-        break
+        result = value * 1_048_576; // Binary: 1 Po = 2^20 Go
+        break;
       case "exaoctet":
-        result = value * 10**9
-        break
+        result = value * 1_073_741_824; // Binary: 1 Eo = 2^30 Go
+        break;
       case "zettaoctet":
-        result = value * 10**12
-        break
+        result = value * 1_099_511_627_776; // Binary: 1 Zo = 2^40 Go
+        break;
       case "yottaoctet":
-        result = value * 10**15
-        break
+        result = value * 1_125_899_906_842_624; // Binary: 1 Yo = 2^50 Go
+        break;
       default:
         throw new Error("Unitée non supportée");
     }
-    return result
+    return result;
   }
-  
-  FromGigaoctet (value, toUnit) { //Convert Go to the selected unit
-    let result = 0
+
+  FromGigaoctet(value, toUnit) { // Convert Go to the selected unit
+    let result = 0;
     switch (toUnit) {
       case "bit":
-        result = value * (8 * 10 ** 9)
+        result = value * (8 * 1_073_741_824); // Binary: 1 Go = 8 * 2^30 bits
         break;
       case "octet":
-        result = value * 10**9
-        break
+        result = value * 1_073_741_824; // Binary: 1 Go = 2^30 octets
+        break;
       case "kilooctet":
-        result = value * 10**6
-        break
+        result = value * (1_024 * 1_024); // Binary: 1 Go = 2^20 kilooctets
+        break;
       case "megaoctet":
-        result = value * 10**3
-        break
+        result = value * 1_024; // Binary: 1 Go = 2^10 megaoctets
+        break;
       case "gigaoctet":
-        result = value
-        break
+        result = value; // Already in gigaoctets
+        break;
       case "terraoctet":
-        result = value / 10**3
-        break
+        result = value / 1_024; // Binary: 1 To = 2^10 Go
+        break;
       case "petaoctet":
-        result = value / 10**6
-        break
+        result = value / 1_048_576; // Binary: 1 Po = 2^20 Go
+        break;
       case "exaoctet":
-        result = value / 10**9
-        break
+        result = value / 1_073_741_824; // Binary: 1 Eo = 2^30 Go
+        break;
       case "zettaoctet":
-        result = value / 10**12
-        break
+        result = value / 1_099_511_627_776; // Binary: 1 Zo = 2^40 Go
+        break;
       case "yottaoctet":
-        result = value / 10**15
-        break
+        result = value / 1_125_899_906_842_624; // Binary: 1 Yo = 2^50 Go
+        break;
       default:
         throw new Error("Unitée non supportée");
     }
-    return result
+    return result;
   }
-  
-  ConvertDonnee(value, fromUnit, toUnit) {
-    let gigaoctet = this.ToGigaoctet(value, fromUnit)
-    return this.FromGigaoctet(gigaoctet, toUnit)
-  }
-}
 
-export { donnees }
+  ConvertDonnee(value, fromUnit, toUnit) {
+    let gigaoctet = this.ToGigaoctet(value, fromUnit);
+    return this.FromGigaoctet(gigaoctet, toUnit);
+  }
+};
+
+export { donnees };
